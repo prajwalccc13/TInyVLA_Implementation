b725b401072fd702827f92ca0ccf4796ef0665e8
Prajwal Chaudhary <prajwalccc13@outlook.com>
2025-04-26 12:59:38 +1000

    removed pythia down

--- llava-pythia/llava_pythia/model/builder.py
+++ llava-pythia/llava_pythia/model/builder.py
@@ -8,6 +8,10 @@ import torch
 from . import *
 # from llava_pythia.model import LlavaPythiaConfig
 # from llava_pythia.model import LlavaPythiaForCausalLM
+
+from llava_pythia.model.language_model.pythia.llava_pythia import LlavaPythiaConfig
+from llava_pythia.model.language_model.pythia.llava_pythia import LlavaPythiaConfig
+
 from llava_pythia.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
 
 
