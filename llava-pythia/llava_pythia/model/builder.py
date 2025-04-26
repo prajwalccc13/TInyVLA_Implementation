600d1925d88522ebed599838f12b767ec6c37367
Prajwal Chaudhary <prajwalccc13@outlook.com>
2025-04-26 12:55:58 +1000

    removed pythia down

--- llava-pythia/llava_pythia/model/builder.py
+++ llava-pythia/llava_pythia/model/builder.py
@@ -5,7 +5,7 @@ import shutil
 from transformers import AutoTokenizer, AutoModelForCausalLM, AutoConfig, BitsAndBytesConfig, CLIPImageProcessor, SiglipImageProcessor, GPTNeoXModel, GPTNeoXPreTrainedModel
     
 import torch
-from llava_pythia.model import *
+from . import *
 # from llava_pythia.model import LlavaPythiaConfig
 # from llava_pythia.model import LlavaPythiaForCausalLM
 from llava_pythia.constants import DEFAULT_IMAGE_PATCH_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
